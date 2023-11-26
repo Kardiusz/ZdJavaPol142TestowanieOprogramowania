ce38247346befffff74823f920290bb6bbc6395e
Bartosz Czerwonogrodzki <bczerwonogrodzki@atlassian.com>
2023-11-26 12:51:27 +0100

    Add historical search - not working due to payment plan

--- src/main/java/org/weatherapp/model/Forecast.java
+++ src/main/java/org/weatherapp/model/Forecast.java
@@ -1,17 +1,25 @@
 package org.weatherapp.model;
 
+import java.util.UUID;
+
 public class Forecast {
 
+    private UUID id;
     private String city;
     private String country;
     private Integer temperature;
     private String weatherDescription;
 
     public Forecast(final String city, final String country, final Integer temperature, final String weatherDescription) {
+        this.id = createUuid(country, city);
         this.city = city;
         this.country = country;
         this.temperature = temperature;
         this.weatherDescription = weatherDescription;
+    }
+
+    public UUID getId() {
+        return id;
     }
 
     public String getCity() {
@@ -30,10 +38,15 @@ public class Forecast {
         return weatherDescription;
     }
 
+    private UUID createUuid(String country, String city) {
+        return UUID.nameUUIDFromBytes((country + city).getBytes());
+    }
+
     @Override
     public String toString() {
         return "Forecast{" +
-                "city='" + city + '\'' +
+                "id=" + id +
+                ", city='" + city + '\'' +
                 ", country='" + country + '\'' +
                 ", temperature=" + temperature +
                 ", weatherDescription='" + weatherDescription + '\'' +
